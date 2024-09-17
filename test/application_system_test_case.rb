a5a6faba460fd354141851c6e3ceaecd045d42a9
whtsht <whiteshirt0079@gmail.com>
2024-09-17 22:30:20 +0900

    create view, path system test

--- test/application_system_test_case.rb
+++ test/application_system_test_case.rb
@@ -1,5 +1,13 @@
 require "test_helper"
 
 class ApplicationSystemTestCase < ActionDispatch::SystemTestCase
-  driven_by :selenium, using: :headless_chrome, screen_size: [ 1400, 1400 ]
+  Capybara.register_driver :selenium_chrome_headless_nosandbox do | app |
+    options = Selenium::WebDriver::Chrome::Options.new;
+    options.add_argument('--headless');
+    options.add_argument('--no-sandbox');
+
+    driver = Capybara::Selenium::Driver.new(app, browser: :chrome, options: options);
+  end
+
+  driven_by :selenium_chrome_headless_nosandbox
 end
