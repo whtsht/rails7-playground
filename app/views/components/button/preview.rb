37d659c2be8d52d530480ad6197c346bb070912a
whtsht <whiteshirt0079@gmail.com>
2024-10-12 09:08:19 +0900

    add button component preview

--- app/views/components/button/preview.rb
+++ app/views/components/button/preview.rb
@@ -4,5 +4,6 @@ class Button::Preview < ApplicationViewComponentPreview
   # You can specify the container class for the default template
   # self.container_class = "w-1/2 border border-gray-300"
 
-  def default; end
+  def primary; end
+  def secondary; end
 end
