127ea450e98d8fa6518bf2c42bc90e5a3e7221fa
whtsht <whiteshirt0079@gmail.com>
2024-12-15 00:12:24 +0900

    flush message without hotwire

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,3 +1,5 @@
-// Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
-import "@hotwired/turbo-rails"
-//import "./controllers"
+import "./controllers"
+
+// The two following lines disable Turbo on the whole application
+import { Turbo } from "@hotwired/turbo-rails"
+Turbo.session.drive = false
