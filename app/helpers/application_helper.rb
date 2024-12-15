2094fee01985aaacbe4dc7ac9c4d11262d1ed717
whtsht <whiteshirt0079@gmail.com>
2024-12-15 09:41:52 +0900

    flush message with hotwire

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,5 @@
 module ApplicationHelper
+  def render_flash_message
+    turbo_stream.prepend "flash", partial: "layouts/flash"
+  end
 end
--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,5 +1,4 @@
-import "./controllers"
+import "controllers"
 
 // The two following lines disable Turbo on the whole application
-import { Turbo } from "@hotwired/turbo-rails"
-Turbo.session.drive = false
+import "@hotwired/turbo-rails"
